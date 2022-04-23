a32349f8213e02aac3ad226a14a5a0f9d5b5d099
haibkhn <aion_2000@yahoo.com.vn>
2022-04-23 16:24:26 +0700

    voronoi surround

--- Voronoi/main.py
+++ Voronoi/main.py
@@ -1,12 +1,8 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Point:
-    x: float
-    y: float
-
-
+from dis import dis
+from shapely.geometry import Point
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import matplotlib.pyplot as plt
+import numpy as np
 obstacle_list = []
 
 
@@ -19,13 +15,42 @@ def input():
             obstacle = []
         else:
             point = line.split()
-            obstacle.append(Point(point[0], point[1]))
+            # print(type(float(point[0])))
+            obstacle.append(Point(float(point[0]), float(point[1])))
+
+
+def distance(p1, p2):
+    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+
+def getEquidistantPoints(p1, p2):
+    parts = int(distance(p1, p2)) * 10
+    return zip(np.linspace(p1[0], p2[0], parts),
+               np.linspace(p1[1], p2[1], parts))
 
 
 if __name__ == "__main__":
     input()
-    print(obstacle_list)
-    # p = Point(5, 5)
-    # obstacle = []
-    # obstacle.append(p)
-    # print(obstacle[0].x)
+
+    vor_points = []
+    # points = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2],
+    #           [2, 0], [2, 1], [2, 2]]
+
+    for obstacle in obstacle_list:
+        for point in obstacle:
+            vor_points.append((point.x, point.y))
+            # xs.append(point.x)
+            # ys.append(point.y)
+        n = len(obstacle)
+        for i in range(n - 1):
+            vor_points += getEquidistantPoints(
+                ((obstacle[i].x, obstacle[i].y)), ((obstacle[i+1].x, obstacle[i+1].y)))
+        vor_points += getEquidistantPoints(
+            ((obstacle[0].x, obstacle[0].y)), ((obstacle[n-1].x, obstacle[n-1].y)))
+    # xs = [i[0] for i in vor_points]
+    # ys = [i[1] for i in vor_points]
+    # print(vor_points)
+    # plt.scatter(xs, ys)
+    vor = Voronoi(vor_points)
+    fig = voronoi_plot_2d(vor)
+    plt.show()
