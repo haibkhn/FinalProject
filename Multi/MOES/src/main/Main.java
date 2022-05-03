4c86837ad9bda0c4399b445936f5b723b3931a1e
haibkhn <aion_2000@yahoo.com>
2022-05-04 00:25:26 +0700

    update more test

--- Multi/MOES/src/main/Main.java
+++ Multi/MOES/src/main/Main.java
@@ -22,7 +22,7 @@ public class Main {
 		// long time = System.currentTimeMillis();
 		// String FILE_URL = "../mopso_test3.txt";
 		// File file = new File(FILE_URL);
-		String numberTeString = "14";
+		String numberTeString = "13";
 		// Tao moi truong
 		GUIRobotics gui = new GUIRobotics(800, 100, 10);
 		gui.generateEnvironment("obstacle_" + numberTeString + ".txt");
@@ -37,7 +37,7 @@ public class Main {
 		ArrayList<Point> resultCombination = new ArrayList<Point>();
 		// ArrayList<Point> resultPareto = new ArrayList<Point>();
 
-		ES es = new ES(8, pointsToVisit.get(0), pointsToVisit.get(1), graph);
+		ES es = new ES(10, pointsToVisit.get(0), pointsToVisit.get(1), graph);
 		try {
 			es.run();
 			resultDistance.add(pointsToVisit.get(0));
