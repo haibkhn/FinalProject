c47f76fe71959564054e20df901de11d8bb94e0d
haibkhn <aion_2000@yahoo.com.vn>
2022-05-26 17:07:20 +0700

    update vor dijkstra

--- Voronoi/main.py
+++ Voronoi/main.py
@@ -1,17 +1,19 @@
 from cmath import inf
 from shapely.geometry import Point, Polygon, MultiPoint
 from shapely.ops import voronoi_diagram
-
+from dijkstar import Graph, find_path
+from dijkstar.algorithm import PathInfo
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
 import numpy as np
 
-INPUT = 2
+INPUT = 18
 
 
 def inputObs(obstacle_list):
-    f = open("obs/input" + str(INPUT) + ".txt", "r")
+    # f = open("obs/input" + str(INPUT) + ".txt", "r")
     # f = open("obs/obstacles.txt", "r")
+    f = open("input/obstacle_" + str(INPUT)+".txt", "r")
     obstacle = []
     for line in f:
         if line == '-1' or line == "-1\n":
@@ -19,16 +21,24 @@ def inputObs(obstacle_list):
             obstacle = []
         else:
             point = line.split()
-            # obstacle.append(Point(float(point[0])/10, float(point[1])/10))
-            obstacle.append(Point(float(point[0]), float(point[1])))
+            obstacle.append(Point(float(point[0])/10, float(point[1])/10))
+            # obstacle.append(Point(float(point[0]), float(point[1])))
+
+    obstacle_list.pop(0)
+    # print(obstacle_list)
+    sur = [Point(0, 0), Point(0, 10), Point(10, 10), Point(10, 0)]
+    obstacle_list.append(sur)
     return obstacle_list
 
 
 def inputTarget(target_list):
-    # f = open("target/target" + str(INPUT) + ".txt", "r")
-    # for line in f:
-    #     point = line.split()
-    #     target_list.append(Point(float(point[0]), float(point[1])))
+    f = open("input/input_" + str(INPUT) + ".txt", "r")
+    for line in f:
+        if line == '-1' or line == "-1\n":
+            return target_list
+        else:
+            point = line.split()
+            target_list.append(Point(float(point[0])/10, float(point[1])/10))
     return target_list
 
 
@@ -41,7 +51,7 @@ def shapelyPoint(p):
 
 
 def getEquidistantPoints(p1, p2):
-    parts = int(distance(p1, p2)*10)
+    parts = int(distance(p1, p2)*12)
     x = np.linspace(p1[0], p2[0], parts, endpoint=False)
     x = x[1:]
     y = np.linspace(p1[1], p2[1], parts, endpoint=False)
@@ -89,7 +99,9 @@ if __name__ == "__main__":
         for p in vor_check:
             if shapelyPoint([p[0], p[1]]).within(polygon):
                 p[2] = 1
+    fig = voronoi_plot_2d(vor)
 
+    # Find closest points to all target
     target_list_closest_index = [0] * len(target_list)
     i = 0
     for target in target_list:
@@ -103,10 +115,54 @@ if __name__ == "__main__":
             j += 1
         i += 1
 
-    fig = voronoi_plot_2d(vor)
+    def distance(v1, v2):
+        return np.sqrt(np.sum((v1 - v2) ** 2))
+
+    # khởi tạo 1 cái graph
+    # kiểm tra cái ridge_vertices đều >= 0 thì thêm vào edge vào graph
+    # mình phải thêm edge [0][1] và edge [1][0] để tạo thành đồ thị vô hướng
+    # còn nếu chỉ thêm edge 1 lần thì nó là đồ thị có hướng
+    graph = Graph()
+    for vpair in vor.ridge_vertices:
+        if vpair[0] >= 0 and vpair[1] >= 0 and (vor_check[vpair[0]][2] != 1) and (vor_check[vpair[1]][2] != 1):
+            graph.add_edge(vpair[0], vpair[1], distance(
+                vor.vertices[vpair[0]], vor.vertices[vpair[1]]))
+            graph.add_edge(vpair[1], vpair[0], distance(
+                vor.vertices[vpair[0]], vor.vertices[vpair[1]]))
+    # điểm bắt đầu và kết thúc trong số vertices của voronoi
+    # ở đây mình sẽ tìm node gần nhất với start và node gần nhất với end
+    start = [target_list[0].x, target_list[0].y]
+    end = [target_list[1].x, target_list[1].y]
+
+    start_graph = 0
+    end_graph = 0
+    min_dist_start = 100000
+    min_dist_end = 100000
+    for i in range(len(vor_vertice)):
+        if (distance(start, vor_vertice[i]) < min_dist_start) and (vor_check[i][2] != 1):
+            min_dist_start = distance(start, vor_vertice[i])
+            start_graph = i
+        if (distance(end, vor_vertice[i]) < min_dist_end) and (vor_check[i][2] != 1):
+            min_dist_end = distance(end, vor_vertice[i])
+            end_graph = i
+
+    # nối từ điểm start và end đến điểm gần nhất trong voronoi vertices
+    plt.plot([start[0], vor_vertice[start_graph][0]], [
+             start[1], vor_vertice[start_graph][1]], 'k', linewidth=2)
+    plt.plot([end[0], vor_vertice[end_graph][0]], [
+             end[1], vor_vertice[end_graph][1]], 'k', linewidth=2)
+
+    # liệt kê các đỉnh đi qua từ start_graph đến end_graph
+    node_list = find_path(graph, start_graph, end_graph).nodes
+    print(node_list)
+    print(vor_vertice[node_list[0]])
+    for i in node_list:
+        plt.plot([vor.vertices[i][0]], [vor.vertices[i][1]],
+                 marker='o', markersize=5, color="red")
+
     plt.scatter(xs, ys, c="r")
     for i in range(len(target_list)):
         plt.plot([target_list[i].x, target_list_closest_index[i][0]], [
-            target_list[i].y, target_list_closest_index[i][1]], color="red", linewidth=3)
-    plt.axis([0, 12, 0, 12])
+            target_list[i].y, target_list_closest_index[i][1]], color="red", linewidth=5)
+    plt.axis([0, 10, 0, 10])
     plt.show()
